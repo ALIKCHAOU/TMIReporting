5cd04957e3d3ca201e73f82f007ada057ad96380
Alii Kchaou <alikchaoufss2@outlook.com>
2022-01-04 15:04:02 +0100

    Update Classe

---
+++ TMIReporting/Model/FicheInformation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMIReporting.Model
+{
+  public  class FicheInformation
+    {
+        public String CodeTitre { get; set; }
+        public String CodeAgence { get; set; }
+        public String CodeIntermediaireAgree { get; set; }
+        public String IntermediaireAgree { get; set; }
+        public String Agence { get; set; }
+        public String Numero3 { get; set; }
+        public String Annee3 { get; set; }
+        public String DateEcheance4 { get; set; }
+        public String TypeBenéficiare5 { get; set; }
+        public String CodeBenéficiare5 { get; set; }
+        public String CodePays { get; set; }
+        public String CodeNaturesOperation10 { get; set; }
+        public String NumerosEcheances6 { get; set; }
+        public String CodeDevises { get; set; }
+        public String CodeSourcesReglements8 { get; set; }
+        public String CodeModesReglements9 { get; set; }
+        public String CodeDemendeur5 { get; set; }
+        public String TypeDemendeur5 { get; set; }
+        public String DossierReference3 { get; set; }
+        public String NomPrenomBeneficiare { get; set; }
+        public String NationaliteBeneficiare { get; set; }
+        public String AdresseBeneficiaire { get; set; }
+        public String PaysDestination { get; set; }
+        public String Montant7 { get; set; }
+        public String NomPrenomDemandeur { get; set; }
+        public String AdresseDemandeur { get; set; }
+        public String NumeroTransfert11 { get; set; }
+        public String DateTransfert { get; set; }
+        public String Modereglements9 { get; set; }
+        public String Sourcereglements8 { get; set; }
+        public String LibellesnaturesOperations10 { get; set; }
+    }
+}

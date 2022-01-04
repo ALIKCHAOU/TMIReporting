56cc5fd5ce7088c3fd870d618fa6041309303b66
Alii Kchaou <alikchaoufss2@outlook.com>
2022-01-04 16:05:49 +0100

    Update Moulinette

---
+++ TMIReporting/Model/OrderTransferEtranger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMIReporting.Model
+{
+   public class OrderTransferEtranger
+    {
+        public String ReferenceClient { get; set; }
+        public String ReferenceAgence { get; set; }
+        public String ReferenceSBE { get; set; }
+        public String CodeBanque { get; set; }
+        public String CodeGuichet { get; set; }
+        public String NumeroCompte { get; set; }
+        public String CleRIB { get; set; }
+        public String CodeDevise { get; set; }
+        public String Nom { get; set; }
+        public String Adresse { get; set; }
+        public String Email { get; set; }
+        public String ModeReglement { get; set; }
+        public String ModeTransfert { get; set; }
+        public String Montant { get; set; }
+        public String ReparationFrais { get; set; }
+        public String MotifPaiement { get; set; }
+        public String NomBeneficiaire { get; set; }
+        public String AdresseBenificiaire { get; set; }
+        public String CodePostalBeneficiaire { get; set; }
+        public String VilleBeneficiaire { get; set; }
+        public String PaysBinificiaire { get; set; }
+        public String NumeroCompteBeneficiaire { get; set; }
+        public String NomBanqueBeneficiaire { get; set; }
+        public String VilleBanqueBeneficiaire { get; set; }
+        public String PaysBanqueBeneficiaire { get; set; }
+        public String AdresseSWIFTBanque { get; set; }
+        public String LibelleDossierRegle { get; set; }
+        public String CodeDossierRegle { get; set; }
+        public String NumeroDossierRegle { get; set; }
+        public String DateDossierRegle { get; set; }
+    }
+}
---
+++ TMIReporting/Model/OrdreTransfertEtrangerOperationCommerciale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMIReporting.Model
+{
+   public class OrdreTransfertEtrangerOperationCommerciale
+    {
+        public String Numero { get; set; }
+        public String ReglementRemiseDoc { get; set; }
+        public String ReglementRemiseEffet { get; set; }
+        public String ReglementFacture { get; set; }
+        public String ReglementAvance { get; set; }
+        public String NumeroCompteDebiter { get; set; }
+        public String CodeDevise { get; set; }
+        public String NomDonneurOrdre { get; set; }
+        public String CodeDouane { get; set; }
+        public String RegistreCommerce { get; set; }
+        public String AdresseDonneurOrdre { get; set; }
+        public String NomBanqueBeneficiaire { get; set; }
+        public String VilleBanque { get; set; }
+        public String PaysBanque { get; set; }
+        public String SWIFTBanque { get; set; }
+        public String TitulaireCompte { get; set; }
+        public String MotifPaiement { get; set; }
+        public String FraisCommissionsBIATCharge { get; set; }
+        public String FraisCorrespondantCharge { get; set; }
+        public String CodeTitreCommerceExterieur { get; set; }
+        public String NumeroTitreCommerceExterieur { get; set; }
+        public String DateTitreCommerceExterieur { get; set; }
+        public String TypeVirment { get; set; }
+    }
+}
